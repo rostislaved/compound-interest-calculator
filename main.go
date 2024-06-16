1c36b1e40ae10849d3cbca6c192a5b839e1a933c
rostislav.teryaev <rostislaved@gmail.com>
2024-06-16 18:06:13 +0300

    dev

--- main.go
+++ main.go
@@ -7,81 +7,80 @@ import (
 )
 
 func main() {
-	c := 3
+	//c := 3
+	//
+	//var baseConfig1 config.BaseConfig
+	//
+	//switch c {
+	//case 1:
+	//	cfg := config.BaseConfig{
+	//		StartAmount:     9000.,
+	//		NumberOfPeriods: 10 * 365 * 6,
+	//		Percent:         0.005,
+	//		PercentEveryN:   1,
+	//		Deposit:         0.,
+	//		DepositEveryN:   1, // Каждые сколько периодов делается deposit
+	//		ReinvestEveryN:  1,
+	//	}
+	//
+	//	baseConfig1 = cfg.GetBaseConfig()
+	//case 2:
+	//	cfg := config.BaseConfig{
+	//		StartAmount:     9000.,
+	//		NumberOfPeriods: 10 * 365 * 6,
+	//		Percent:         0.005,
+	//		PercentEveryN:   1,
+	//		Deposit:         0.,
+	//		DepositEveryN:   1, // Каждые сколько периодов делается deposit
+	//		ReinvestEveryN:  6 * 60,
+	//	}
+	//
+	//	baseConfig1 = cfg.GetBaseConfig()
+	//case 3:
+	//	// Этот конфиг как на сайте
+	//	cfg := config.CommonConfig{
+	//		StartAmount:          9000.,
+	//		DurationOfInvestment: 10 * config.Year,
+	//		Percent:              12., // Годовых
+	//
+	//		Deposit:       100,
+	//		DepositEveryN: config.Month,
+	//
+	//		ReinvestEveryN: 6 * config.Month,
+	//	}
+	//
+	//	baseConfig1 = cfg.GetBaseConfig()
+	//default:
+	//	return
+	//}
 
-	var baseConfig1 config.BaseConfig
-
-	switch c {
-	case 1:
-		cfg := config.BaseConfig{
-			StartAmount:     9000.,
-			NumberOfPeriods: 10 * 365 * 6,
-			Percent:         0.005,
-			PercentEveryN:   1,
-			Deposit:         0.,
-			DepositEveryN:   1, // Каждые сколько периодов делается deposit
-			ReinvestEveryN:  1,
-		}
-
-		baseConfig1 = cfg.GetBaseConfig()
-	case 2:
-		cfg := config.BaseConfig{
-			StartAmount:     9000.,
-			NumberOfPeriods: 10 * 365 * 6,
-			Percent:         0.005,
-			PercentEveryN:   1,
-			Deposit:         0.,
-			DepositEveryN:   1, // Каждые сколько периодов делается deposit
-			ReinvestEveryN:  6 * 60,
-		}
-
-		baseConfig1 = cfg.GetBaseConfig()
-	case 3:
-		// Этот конфиг как на сайте
-		cfg := config.CommonConfig{
-			StartAmount:          9000.,
-			DurationOfInvestment: 10 * config.Year,
-			Percent:              12., // Годовых
-
-			Deposit:       100,
-			DepositEveryN: config.Month,
-
-			ReinvestEveryN: 6 * config.Month,
-		}
-
-		baseConfig1 = cfg.GetBaseConfig()
-	default:
-		return
+	baseConfig1 := config.BaseConfig{
+		StartAmount:     9000.,
+		NumberOfPeriods: 10 * 365 * 6,
+		Percent:         0.005,
+		PercentEveryN:   1,
+		Deposit:         0.,
+		DepositEveryN:   1, // Каждые сколько периодов делается deposit
+		ReinvestEveryN:  1,
 	}
 
-	//baseConfig1 := config.BaseConfig{
-	//	StartAmount:     9000.,
-	//	NumberOfPeriods: 20 * 365 * 6,
-	//	Percent:         0.005,
-	//	PercentEveryN:   1,
-	//	Deposit:         0.,
-	//	DepositEveryN:   1, // Каждые сколько периодов делается deposit
-	//	ReinvestEveryN:  1,
-	//}
-	//
-	//baseConfig2 := config.BaseConfig{
-	//	StartAmount:     9000.,
-	//	NumberOfPeriods: 20 * 365 * 6,
-	//	Percent:         0.005,
-	//	PercentEveryN:   1,
-	//	Deposit:         0.,
-	//	DepositEveryN:   1, // Каждые сколько периодов делается deposit
-	//	ReinvestEveryN:  6 * 365,
-	//}
+	baseConfig2 := config.BaseConfig{
+		StartAmount:     9000.,
+		NumberOfPeriods: 10 * 365 * 6,
+		Percent:         0.005,
+		PercentEveryN:   1,
+		Deposit:         0.,
+		DepositEveryN:   1, // Каждые сколько периодов делается deposit
+		ReinvestEveryN:  6 * 30,
+	}
 
 	calculation1 := New(baseConfig1)
-	//calculation2 := New(baseConfig2)
+	calculation2 := New(baseConfig2)
 
 	result1 := calculation1.Calc()
-	//result2 := calculation2.Calc()
+	result2 := calculation2.Calc()
 
-	//PrintStatsByPeriod(result)
 	fmt.Println()
-	PrintStatsByPeriod(result1)
-	PrintStats(result1)
+	PrintStatsByPeriod(result2)
+	PrintStats(result1, result2)
 }
--- printing.go
+++ printing.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
 func PrintStats(results ...Result) {
-	var blocks1 []string
-	var blocks2 []string
-	var blocks3 []string
-	var blocks4 []string
+	var (
+		blocks1 []string
+		blocks2 []string
+		blocks3 []string
+		blocks4 []string
+		blocks5 []string
+		blocks6 []string
+	)
+
+	maxPercentSum := math.MaxFloat64
+	maxLastAmount := math.MaxFloat64
+
+	for _, result := range results {
+		lastAmount := result.LastAmount()
+		lastPeriod := result.LastPeriod()
+
+		maxPercentSum = min(maxPercentSum, lastPeriod.percentSum)
+		maxLastAmount = min(maxLastAmount, lastAmount)
+	}
 
 	for _, result := range results {
 		startAmount := result.FirstPeriod().startAmount
@@ -21,29 +37,40 @@ func PrintStats(results ...Result) {
 		block2 := fmt.Sprintf("%.2f (%.1f%%) (%.1f%%)", lastPeriod.depositSum, getPercent(lastPeriod.depositSum, lastAmount), getPercent(lastPeriod.depositSum, allDeposits))
 		block3 := fmt.Sprintf("%.2f (%.1f%%) (%.1f%%)", lastPeriod.percentSum, getPercent(lastPeriod.percentSum, lastAmount), getPercent(lastPeriod.percentSum, allDeposits))
 		block4 := fmt.Sprintf("%.2f (100%%) (%.1f%%)", lastAmount, getPercent(lastAmount, allDeposits))
+		block5 := fmt.Sprintf("%.2f (%.1f%%)", lastPeriod.percentSum-maxPercentSum, getPercent(lastPeriod.percentSum, maxPercentSum))
+		block6 := fmt.Sprintf("%.2f (%.1f%%)", lastAmount-maxLastAmount, getPercent(lastAmount, maxLastAmount))
 		length := max(len(block1), len(block2), len(block3), len(block4))
 
 		block1 = complementWithSpaces(block1, length)
 		block2 = complementWithSpaces(block2, length)
 		block3 = complementWithSpaces(block3, length)
 		block4 = complementWithSpaces(block4, length)
+		block5 = complementWithSpaces(block5, length)
+		block6 = complementWithSpaces(block6, length)
 
 		blocks1 = append(blocks1, block1)
 		blocks2 = append(blocks2, block2)
 		blocks3 = append(blocks3, block3)
 		blocks4 = append(blocks4, block4)
+		blocks5 = append(blocks5, block5)
+		blocks6 = append(blocks6, block6)
 	}
 
 	line1 := fmt.Sprintf("| Start Amount   | %s |", strings.Join(blocks1, " | "))
 	line2 := fmt.Sprintf("| Total Deposits | %s |", strings.Join(blocks2, " | "))
 	line3 := fmt.Sprintf("| Total Income   | %s |", strings.Join(blocks3, " | "))
 	line4 := fmt.Sprintf("| Total Amount   | %s |", strings.Join(blocks4, " | "))
+	line5 := fmt.Sprintf("| Diff Income    | %s |", strings.Join(blocks5, " | "))
+	line6 := fmt.Sprintf("| Diff Amount    | %s |", strings.Join(blocks6, " | "))
 
 	fmt.Println(strings.Repeat("-", len(line1)))
 	fmt.Println(line1)
 	fmt.Println(line2)
 	fmt.Println(line3)
 	fmt.Println(line4)
+	fmt.Println(strings.Repeat("-", len(line1)))
+	fmt.Println(line5)
+	fmt.Println(line6)
 	fmt.Println(strings.Repeat("-", len(line1)))
 	fmt.Println()
 }
